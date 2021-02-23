1c927749e20498f1a3e82a90a04fbb95fc6ea657
gusar312 <76468386+gusar312@users.noreply.github.com>
2021-02-23 19:56:40 +0700

    PHP

---
+++ index.php
@@ -0,0 +1,7 @@
+<a href="lab_1_Опалев_П-811.php">Lab 1</a>
+<br>
+<a href="lab_2_Опалев_П-811.php">Lab 2</a>
+<br>
+<a href="lab_3_Опалев_П-811.php">Lab 3</a>
+<br>
+<a href="lab_4_Опалев_П-811.php">Lab 4</a>
